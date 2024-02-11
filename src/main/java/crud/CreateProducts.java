5ac55f5f4d5fd58b64f667ebe0b1a2f38d022f5f
Dana <Dana.ackerman0914@gmail.com>
2024-02-11 20:23:00 +0600

    Created classes with crud operations create, update and remove using jpql language

---
+++ src/main/java/crud/CreateProducts.java
@@ -0,0 +1,63 @@
+package crud;
+
+import entities.Category;
+import entities.Options;
+import entities.Products;
+import entities.Values;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+import java.util.Scanner;
+
+
+public class CreateProducts {
+    public static void main(String[] args) {
+        //Macbook air
+        //Apple, 13.3,  Apple MacBook Pro 13 A1278
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("main");
+        EntityManager manager = factory.createEntityManager();
+        Scanner scan = new Scanner(System.in);
+        try {
+            manager.getTransaction().begin();
+            System.out.println("Введите номер категории: ");
+            long id = scan.nextLong();
+            scan.nextLine();
+            Category category = manager.find(Category.class, id);
+            if (category == null) {
+                System.out.println("Сущность не найдена");
+            } else {
+                System.out.println("Введите названия товара: ");
+                String createProducts = scan.nextLine();
+                Products products = new Products();
+                products.setName(createProducts);
+                System.out.println("Введите стоимость товара: ");
+                long price = scan.nextLong();
+                scan.nextLine();
+                products.setPrice(price);
+                products.setCategory(category);
+                manager.persist(products);
+
+                List<Options> optionsList = category.getOptions();
+                for (Options o : optionsList) {
+                    System.out.println("Введите значения характеристик: ");
+                    String createValues = scan.nextLine();
+                    Values values = new Values();
+                    values.setName(createValues);
+                    values.setOptions(o);
+                    values.setProducts(products);
+                    manager.persist(values);
+                }
+            }
+            manager.getTransaction().commit();
+        } catch (Exception e) {
+            manager.getTransaction().rollback();
+            throw new RuntimeException(e);
+        }
+        scan.close();
+        manager.close();
+        factory.close();
+    }
+}
+
---
+++ src/main/java/crud/UpdateProduct.java
@@ -0,0 +1,41 @@
+package crud;
+
+import entities.Products;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.Scanner;
+
+public class UpdateProduct {
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("main");
+        EntityManager manager = factory.createEntityManager();
+        Scanner scan = new Scanner(System.in);
+        try {
+            manager.getTransaction().begin();
+            System.out.println("Введите номер товара: ");
+            long productId = scan.nextLong();
+            scan.nextLine();
+            Products product = manager.find(Products.class, productId);
+            if (product != null) {
+                System.out.printf("Введите новое название товара [%s]: ", product.getName());
+                String productName = scan.nextLine();
+                product.setName(productName);
+
+                System.out.printf("Введите новую стоимость товара [%d]: ", product.getPrice());
+                long price = scan.nextLong();
+                product.setPrice(price);
+            } else {
+                System.out.println("Сущность не найдена");
+            }
+            manager.getTransaction().commit();
+        } catch (Exception e) {
+            manager.getTransaction().rollback();
+            throw new RuntimeException(e);
+        }
+        manager.close();
+        factory.close();
+    }
+}
+
